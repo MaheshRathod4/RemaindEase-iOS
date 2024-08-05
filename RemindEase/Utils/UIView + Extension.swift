4f1f48692f82321e2f777a144c0aaa801d3a3d56
MRD <maheshr8484@gmail.com>
2024-08-05 12:50:12 +0530

    update colors

---
+++ RemindEase/Utils/UIView + Extension.swift
@@ -0,0 +1,102 @@
+//
+//  UIView + Extension.swift
+//  RemindEase
+//
+//  Created by MTPC-206 on 05/08/24.
+//
+
+import Foundation
+import UIKit
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CustomizeView:UIView {
+    
+    @IBInspectable
+    var borderColor:UIColor = .clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth:CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius:CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor:UIColor = .clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset:CGSize = .zero {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius:CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity:Float = 0 {
+        didSet {
+            layer.shadowOpacity = shadowOpacity
+        }
+    }
+    
+    @IBInspectable var topLeftCornerRadius:Bool = true {
+        didSet {
+            setCustomCornerRadius()
+        }
+    }
+    @IBInspectable var topRightCornerRadius:Bool = true {
+        didSet {
+            setCustomCornerRadius()
+        }
+    }
+    @IBInspectable var bottomLeftCornerRadius:Bool = true {
+        didSet {
+            setCustomCornerRadius()
+        }
+    }
+    @IBInspectable var bottomRightCornerRadius:Bool = true {
+        didSet {
+            setCustomCornerRadius()
+        }
+    }
+    private func setCustomCornerRadius() {
+        var cornerMask = CACornerMask()
+        if topLeftCornerRadius {
+            cornerMask.insert(.layerMinXMinYCorner)
+        }
+        if topRightCornerRadius {
+            cornerMask.insert(.layerMaxXMinYCorner)
+        }
+        if bottomLeftCornerRadius {
+            cornerMask.insert(.layerMinXMaxYCorner)
+        }
+        if bottomRightCornerRadius {
+            cornerMask.insert(.layerMaxXMaxYCorner)
+        }
+        layer.maskedCorners = cornerMask
+    }
+}
